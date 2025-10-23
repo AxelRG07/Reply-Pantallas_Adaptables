bab4f503aba5b2484a138dfe4529b66ed7c08171
axelr <axelrepeat@gmail.com>
2025-10-22 23:38:00 -0600

    Se agregaron pruebas automatizadas para probar la adaptabilidad,se configuraron las pruebas para ver si la disposición de los elementos era la correcta dependiendo del tamaño de la pantalla y las pruebas para verificar que la pantalla conserva su estado después de un cambio de configuración también fueron añadidas.

--- app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
+++ app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
@@ -190,12 +190,14 @@ private fun ReplyAppContent(
                 AnimatedVisibility(
                     visible = navigationType == ReplyNavigationType.BOTTOM_NAVIGATION
                 ) {
+                    val bottomNavigationContentDescription = stringResource(R.string.navigation_bottom)
                     ReplyBottomNavigationBar(
                         currentTab = replyUiState.currentMailbox,
                         onTabPressed = onTabPressed,
                         navigationItemContentList = navigationItemContentList,
                         modifier = Modifier
                             .fillMaxWidth()
+                            .testTag(bottomNavigationContentDescription)
                     )
                 }
             }
